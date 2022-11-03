0942cd65e5484ac841fce930bfe6b4439bad0f91
Pablo M. Rivera <pablomrivera1976@gmail.com>
2022-11-03 14:17:13 -0400

    updated and does not crash

--- tip_calculator.py
+++ tip_calculator.py
@@ -9,12 +9,11 @@ tip_input = int(input("What Percentage tip would you like to give?: %  "))
 if tip_input >= 10 < 20:
     print("You are a great tipper!")
 else:
-    if tip_input > 20:
+    if tip_input >= 20:
         print("You are the world's greatest tipper!")
     else:
         if tip_input>0<10:
             print("You are so damn cheap!")
-            cheap=int(input("One Last Try for A REAL TIP!:%  "))
         else:
             if tip_input<=0:
                 print("You should be ashamed of yourself. I never met anyone so cheap. I am going to call your mamma!")
@@ -25,17 +24,21 @@ print("\n")
 #Calculate the sales tax
 tax = (bill*.1)
 total_bill=bill+tax
-#Calculate the value each person owes based on the bill and tips the user entered
-payment_per_person = ("%.2f" % round(float((((max(tip_input,cheap)) / 100 +1) * total_bill / num_people)),2))
+total_bill_tip=bill+tax+((tip_input*bill)/100)
+ #(((float(((tip_input)) / 100+1) * total_bill / num_people)),2)
 #Print the $ amount of the tip
-tip_calc = ("%.2f" % float((max(tip_input,cheap)) / 100 * bill))
+tip_calc = ("%.2f" % float((tip_input) / 100 * bill))
 print(f"Tip amount: ${tip_calc}")
+#Calculate the value each person owes based on the bill and tips the user entered
+payment_per_person = (total_bill_tip/num_people)
 #convert total bill and tip amount to floats so they can be added
 total_bill_float = float(bill)
 tip_amount_float = float(tip_calc)
 tip_and_total = (total_bill_float+tip_amount_float)
 print(f"Total bill including tip: ${tip_and_total}")
+print(f"Tax amount': ${tax}")
 #Print what each person needs to pay
+print(f"Total with tax and Tip: $ {total_bill_tip}")
 print(f"Each person owes: ${payment_per_person}")
 
 
